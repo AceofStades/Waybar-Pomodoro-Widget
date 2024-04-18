52316d5be60fb4ef3337c2df1c6ee626945bc54e
AceofStades <rahulpbokade@gmail.com>
2024-04-18 18:54:40 +0530

    Massive updates

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ default_config = {
         "study_sessions": 4,
         "long_break_time": 15 * 60,
         "last_paused_at": 0,            # Placeholder for time when timer was paused
-        "state_running": False,   # Placeholder for checking current state of timer
+        "state_running": False,         # Placeholder for checking current state of timer
         }
 
 config_filename = "config.json"
@@ -18,21 +18,32 @@ config_filename = "config.json"
 def create_default_json(config_filename):
     with open(config_filename, 'w') as config_file:
         json.dump(default_config, config_file, indent= 4)
+    config_file.close()
 
-def countdown(time_in_seconds):
+# Countdown timer
+def countdown(time_in_seconds, state_running):
     while time_in_seconds:
-        mins, secs = divmod(time_in_seconds, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
-        time.sleep(1)
-        time_in_seconds -= 1
-    print("Fire in the hole")
+        if state_running == False:
+            update_last_paused_at(time_in_seconds)
+            break
+        else:
+            try:
+                mins, secs = divmod(time_in_seconds, 60)
+                timer = '{:02d}:{:02d}'.format(mins, secs)
+                print(timer, end="\r")
+                time.sleep(1)
+                time_in_seconds -= 1
+            except KeyboardInterrupt:
+                update_last_paused_at(time_in_seconds)
+                break
 
+# Toggle the timer between running and paused states
 def toggle_timer(state_running, args):
     state_running = not state_running
     args.toggle = False
     return state_running
 
+# Check the current state of the timer
 def check_current_state(args, state_running):
     if args.start:
         state_running = True
@@ -41,6 +52,16 @@ def check_current_state(args, state_running):
     if args.toggle:
         state_running = toggle_timer(state_running, args)
     return state_running
+
+# Updates the last_paused_at time in config.json if the timer is paused
+def update_last_paused_at(time_in_seconds):
+    with open(config_filename, 'r') as config_file_read:
+        config = json.load(config_file_read)
+        config["last_paused_at"] = time_in_seconds
+        with open(config_filename, 'w') as config_file_write:
+            json.dump(config, config_file_write, indent=4)
+        config_file_write.close()
+    config_file_read.close()
 
 def main():
     # Check for file existence and create default config if it doesn't exist
@@ -59,6 +80,7 @@ def main():
     study_sessions = config["study_sessions"]
     long_break_time = config["long_break_time"]
     state_running = config["state_running"]      # True while countdown is running, False when paused
+    last_paused_at = config["last_paused_at"]
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--start", help="Start the timer", action="store_true")
@@ -68,8 +90,11 @@ def main():
 
     state_running = check_current_state(parser.parse_args(), state_running)
 
-    if state_running == True and parser.parse_args().reset == False:
-        countdown(study_time)
+    if parser.parse_args().reset == False:
+        if last_paused_at == 0:
+            countdown(study_time, state_running)
+        else:
+            countdown(last_paused_at, state_running)
 
 if __name__ == "__main__":
     main()
