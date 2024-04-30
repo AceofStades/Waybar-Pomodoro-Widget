e036469cfc1e52d0215cde52b72a0c141719fc27
AceofStades <rahulpbokade@gmail.com>
2024-04-30 19:43:46 +0530

    Added threading

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import time
 import os
 import json
 import argparse
+import threading
+import sys
 
 default_config = {
         "study_time": 25 * 60,
@@ -38,19 +40,31 @@ def countdown(time_in_seconds, state_running):
                 break
 
 # Toggle the timer between running and paused states
-def toggle_timer(state_running, args):
-    state_running = not state_running
-    args.toggle = False
-    return state_running
+# def toggle_timer(state_running, args):
+#     state_running = not state_running
+#     args.toggle = False
+#     return state_running
 
 # Check the current state of the timer
-def check_current_state(args, state_running):
-    if args.start:
+# def check_current_state(args, state_running):
+#     if args.start:
+#         state_running = True
+#     if args.pause:
+#         state_running = False
+#     if args.toggle:
+#         state_running = toggle_timer(state_running, args)
+#     return state_running
+
+def check_current_state(state_queue, state_running, study_time):
+    if state_queue == "s":
         state_running = True
-    if args.pause:
+    if state_queue == "p":
         state_running = False
-    if args.toggle:
-        state_running = toggle_timer(state_running, args)
+    if state_queue == "t":
+        state_running = not state_running
+    if state_running == "r":
+        state_running = False
+        update_last_paused_at(study_time)   # Reset the timer to study_time for now. Will implement reset to current_session_time later
     return state_running
 
 # Updates the last_paused_at time in config.json if the timer is paused
@@ -63,7 +77,7 @@ def update_last_paused_at(time_in_seconds):
         config_file_write.close()
     config_file_read.close()
 
-def main():
+def main(state_queue):
     # Check for file existence and create default config if it doesn't exist
     if not os.path.isfile(config_filename):
         create_default_json(config_filename)
@@ -82,19 +96,25 @@ def main():
     state_running = config["state_running"]      # True while countdown is running, False when paused
     last_paused_at = config["last_paused_at"]
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--start", help="Start the timer", action="store_true")
-    parser.add_argument("-p", "--pause", help="Pause the timer", action="store_true")
-    parser.add_argument("-r", "--reset", help="Reset the timer", action="store_true")
-    parser.add_argument("-t", "--toggle", help="Toggle the timer", action="store_true")
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("-s", "--start", help="Start the timer", action="store_true")
+    # parser.add_argument("-p", "--pause", help="Pause the timer", action="store_true")
+    # parser.add_argument("-r", "--reset", help="Reset the timer", action="store_true")
+    # parser.add_argument("-t", "--toggle", help="Toggle the timer", action="store_true")
 
-    state_running = check_current_state(parser.parse_args(), state_running)
+    # state_running = check_current_state(parser.parse_args(), state_running)
 
-    if parser.parse_args().reset == False:
-        if last_paused_at == 0:
-            countdown(study_time, state_running)
-        else:
-            countdown(last_paused_at, state_running)
+    state_running = check_current_state(state_queue, state_running, study_time)
+
+    if last_paused_at == 0:
+        countdown(study_time, state_running)
+    else:
+        countdown(last_paused_at, state_running)
 
 if __name__ == "__main__":
-    main()
+    state_queue = queue.Queue()
+    threading.Thread(target=main, daemon=True, args=(state_queue,)).start()
+    while True:
+        state = char(input())
+        if state in ("s", "p", "r", "t"):
+            state_queue.put(state)
